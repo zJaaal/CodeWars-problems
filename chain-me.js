e5a77a9b8a54fb8ce50adce52475490b0599653c
Jalinson Diaz <zjaaaldev@gmail.com>
2022-06-01 16:13:47 -0300

    Last push to 5 kyu!

---
+++ chain-me.js
@@ -0,0 +1,9 @@
+//7 kyu Problem Chain Me
+// https://www.codewars.com/kata/54fb853b2c8785dd5e000957/
+function chain(input, fs) {
+    let result =[input];
+    fs.forEach((f,i) =>{
+      result.push(f(result[i])); 
+    });
+    return result.pop();
+}
---
+++ scramble.js
@@ -0,0 +1,10 @@
+//7 kyu Problem Scramble
+//https://www.codewars.com/kata/5822d89270ca28c85c0000f3
+
+function scramble(str, arr) {
+    let newStr = new Array(str.length);
+    arr.forEach((pos, i) =>{
+      newStr[pos] = str[i];
+    });
+    return newStr.join("");
+};
