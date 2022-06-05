85a2f1fce499526df279fbd4a72c19c6c46c242b
Jalinson Diaz <zjaaaldev@gmail.com>
2022-06-05 18:09:16 -0300

    60% of the way to 4 kyu

---
+++ strip-comments.js
@@ -0,0 +1,13 @@
+// 4 kyu Problem Strip Comments
+//https://www.codewars.com/kata/51c8e37cee245da6b40000bd
+
+
+function solution(input, markers) {
+    markers.forEach(element => {
+        let regex = new RegExp(`(\\s)\\${element}(\\s)?\\w\.\*\$`, "gm");
+        input = input.replace(regex, "");
+    });
+    return input;
+};
+
+console.log(solution("apples, plums % and bananas\npears\noranges !applesauce", ["%", "!"]));
