df10f40fecbd8e11a85e9f0010f016297c49e89b
Jalinson Diaz <zjaaaldev@gmail.com>
2022-06-11 22:27:05 -0300

    Just for fun :)

---
+++ 4_Kyu/vigenere-cipher.js
@@ -0,0 +1,39 @@
+//4 kyu Problem Vigenère Cipher Helper
+//https://www.codewars.com/kata/52d1bd3694d26f8d6e0000d3
+
+function VigenèreCipher(key, abc) {
+    this.encode = function (str) {
+      let keyChar = -1;
+       str = str.split("").map((char) =>{
+          keyChar = ++keyChar > key.length - 1 ? 0 : keyChar;
+          let abcChar = abc.indexOf(char);
+          if(abcChar == -1) return char;
+          let encodeChar = abc.indexOf(key[keyChar]);
+          let result = abc.indexOf(char) + encodeChar > abc.length - 1 ?  abc.indexOf(char) + encodeChar - abc.length :  abc.indexOf(char) + encodeChar;
+          return abc[result];
+        }).join("");
+
+        return str;
+    };
+    this.decode = function (str) {
+      let keyChar = -1;
+      str = str.split("").map((char) =>{
+        keyChar = ++keyChar > key.length -1 ? 0: keyChar;
+        let abcChar = abc.indexOf(char);
+        if(abcChar == -1) return char;
+        let encodeChar = abc.indexOf(key[keyChar]);
+        let result = abc.indexOf(char) - encodeChar < 0 -1 ?  abc.indexOf(char) - encodeChar + abc.length:  abc.indexOf(char) - encodeChar;
+        return abc[result];
+      }).join("");
+      
+      return str;
+    };
+}
+
+var abc, key;
+abc = "abcdefghijklmnopqrstuvwxyz";
+key = "pizza"
+c = new VigenèreCipher(key, abc);
+
+console.log(c.encode("asodavwt"));
+console.log(c.decode("pancakes"));
