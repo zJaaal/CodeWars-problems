121a1857a59a3477e2dd0e342255c386716d5f1d
Jalinson Diaz <zjaaaldev@gmail.com>
2022-09-30 17:03:55 -0300

    Create sql.js

---
+++ 1_Kyu/sql.js
@@ -0,0 +1,4 @@
+//https://www.codewars.com/kata/545434090294935e7d0010ab
+//1 Kyu Problem Functional SQL
+
+// I did solved it but I wont show the solution since it's one of the highest difficult and would be easy points if you found this.
