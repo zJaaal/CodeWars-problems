cf5a76014fe26069af2bdea3db49eafbb7cc2343
Jalinson Diaz <zjaaaldev@gmail.com>
2022-10-11 19:34:55 -0300

    Some infinite scroll i made for my work

---
+++ misc/wordCarousel.js
@@ -0,0 +1,12 @@
+const carouselSection = document.querySelector('.word-carousel>div>div');
+
+carouselSection.innerHTML = `
+<h2>Let's Talk&nbsp;·&nbsp;</h2>
+<h2>Hablemos&nbsp;·&nbsp;</h2>
+<h2>Parlem&nbsp;·&nbsp;</h2>
+<h2>Parlons</h2>
+<h2>&nbsp;·&nbsp;Let's talk&nbsp;·&nbsp;</h2>
+<h2>Hablemos&nbsp;·&nbsp;</h2>
+<h2>Parlem&nbsp;·&nbsp;</h2>
+<h2>Parlons&nbsp;·&nbsp;</h2>
+`;
