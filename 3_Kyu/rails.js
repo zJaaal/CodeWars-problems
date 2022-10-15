8a589a946184cc4b9921cb192d0dee3de9a68d57
Jalinson Diaz <zjaaaldev@gmail.com>
2022-10-15 13:43:58 -0300

    Create rails.js

---
+++ 3_Kyu/rails.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/58c5577d61aefcf3ff000081
+
+function encodeRailFenceCipher(string, numberRails) {}
+
+function decodeRailFenceCipher(string, numberRails) {
+  // code
+}
+
+console.clear();
+console.log(encodeRailFenceCipher('WEAREDISCOVEREDFLEEATONCE', 3)); //"WECRLTEERDSOEEFEAOCAIVDEN" //WDVFTEEIOEDLAOEARSCREEENC
+console.log(decodeRailFenceCipher('WECRLTEERDSOEEFEAOCAIVDEN', 3));
