62e03ad982e90e511e392908ac5f878068de56be
Jalinson Diaz <zjaaaldev@gmail.com>
2022-12-05 12:21:06 -0300

    better solutions

--- ajs/day1.js
+++ ajs/day1.js
@@ -1,12 +1,8 @@
 console.clear();
 function wrapping(gifts) {
-  let dictionary = new Map();
-  return gifts.map((gift) => {
-    if (!dictionary.get(gift))
-      dictionary.set(gift, '*'.repeat(gift.length + 2));
-
-    return `${dictionary.get(gift)}\n*${gift}*\n${dictionary.get(gift)}`;
-  });
+  return gifts.map(
+    (gift, i) => ((i = '*'.repeat(gift.length + 2)), `${i}\n*${gift}*\n${i}`)
+  );
 }
 
 console.log(wrapping(['book', 'game', 'socks']));
--- ajs/day2.js
+++ ajs/day2.js
@@ -1,8 +1,11 @@
 function countHours(year, holidays) {
-  return holidays.reduce((count, day) => {
-    let weekDay = new Date(day + '/' + year).getDay();
-    return weekDay > 0 && weekDay < 6 ? (count += 2) : count;
-  }, 0);
+  return holidays.reduce(
+    (count, day, i) => (
+      (i = new Date(day + '/' + year).getDay()),
+      i > 0 && i < 6 ? (count += 2) : count
+    ),
+    0
+  );
 }
 
 console.log(countHours(2020, ['01/06', '04/01', '12/31']));
