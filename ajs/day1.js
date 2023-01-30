d1182e7ffab2d3e57f592ce14803a3b8caa1540e
Jalinson Diaz <zjaaaldev@gmail.com>
2023-01-30 15:01:52 -0300

    cleaning this

--- ajs/day1.js
+++
@@ -1,8 +0,0 @@
-console.clear();
-function wrapping(gifts) {
-  return gifts.map(
-    (gift, i) => ((i = '*'.repeat(gift.length + 2)), `${i}\n*${gift}*\n${i}`)
-  );
-}
-
-console.log(wrapping(['book', 'game', 'socks']));
