6472f8a79e56175dea16a0b8986559f716a003f6
Jalinson Diaz <zjaaaldev@gmail.com>
2023-02-10 14:30:27 -0300

    6 kyu kata

---
+++ 6_Kyu/stringToLinkedList.js
@@ -0,0 +1,25 @@
+function parse(string) {
+  let data = string.split('->').map((x) => +x.trim());
+
+  if (isNaN(data[0])) return null;
+
+  let root = new Node(data.shift());
+  createNode(data, root);
+
+  return root;
+}
+
+function createNode(data, node) {
+  let currentData = data.shift();
+
+  if (data.length == 0) {
+    node.next = null;
+    return node;
+  }
+
+  if (!isNaN(currentData)) {
+    let currentNode = new Node(currentData);
+    node.next = currentNode;
+    return createNode(data, currentNode);
+  }
+}
